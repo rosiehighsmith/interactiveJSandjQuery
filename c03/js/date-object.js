ad84a38f14797a85159ddddb85c4d1e86f6cbdff
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-02-03 09:52:10 -0800

    math and date objects

--- c03/js/date-object.js
+++ c03/js/date-object.js
@@ -1,1 +1,1 @@
-// Create a variable to hold a new Date object (defaults to now)var today = new Date();// Create a variable to hold the year this yearvar year = today.getFullYear();// Create a variable called el to hold the element whose id attribute has a value of footervar el = document.getElementById('footer');// Write the year into that element.el.innerHTML = '<p>Copyright &copy;' + year + '</p>';
+var today = new Date();var year = today.getFullYear();var el = document.getElementById('footer');el.innerHTML = '<p>Copyright @copy;' + year + '</p>';
