cd19eebc9a335648aa2ccef9395c9c7f0a0e8a83
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-02-16 08:30:45 -0800

    do while and example loop

--- c04/js/example.js
+++ c04/js/example.js
@@ -1,22 +1,18 @@
-var table = 3;             // Unit of table
-var operator = 'addition'; // Type of calculation
-var i = 1;                 // Set counter to 1
-var msg = '';              // Message
+var table = 3;
+var operator = 'addition';
+var i = 1;
+var msg = '';
 
 if (operator === 'addition') {
-  // Do addition
   while (i < 11) {
     msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
     i++;
   }
 } else {
-  // Do multiplication
   while (i < 11) {
     msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
     i++;
   }
 }
 
-// Write the message into the page
-var el = document.getElementById('blackboard');
-el.innerHTML = msg;
+document.getElementById('blackboard').innerHTML = msg;
